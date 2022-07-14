9d1a8d924786eead42faf3965b530b415dc7c06e
Akash2774 <96769627+Akash2774@users.noreply.github.com>
2022-07-14 15:32:38 +0530

    #include<stdio.h> int main() {     int n,i,s=0;     scanf("%d",&n);     i=1;     while(i<n)     {         if(n%i==0)         s=s+i;         i++;     }     if(s==n)     {         printf("True");     }     else     {         printf("False");     } }

--- Perfect_Number.c
+++ Perfect_Number.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,sum=0;
+    int n,i,s=0;
     scanf("%d",&n);
     i=1;
     while(i<n)
     {
         if(n%i==0)
-        sum=sum+i;
+        s=s+i;
         i++;
     }
-    if(sum==n)
+    if(s==n)
     {
         printf("True");
     }
